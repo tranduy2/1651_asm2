ca8aec60d7ec17bc950c2edcb2c63198a28ca2cc
tranduy2 <137510397+tranduy2@users.noreply.github.com>
2023-10-26 20:05:15 +0700

    Add files via upload

---
+++ ASM/LibraryItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASM
+{
+    public class LibraryItem
+    {
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && value.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Length <= 80)
+                {
+                    title = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid title. Please submit a title of no more than 80 words.");
+                }
+            }
+        }
+
+        private string author;
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value) && !value.Any(char.IsDigit) && value.Length <= 40)
+                {
+                    author = value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid author name. Please submit an author name with no numbers and a maximum of 40 characters.");
+                }
+            }
+        }
+
+        public bool IsAvailable { get; set; } = true;
+
+        public LibraryItem(string title, string author)
+        {
+            Title = title;
+            Author = author;
+        }
+    }
+
+}
